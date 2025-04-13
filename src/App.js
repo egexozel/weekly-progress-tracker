742fb1fb446a22bcc2713dab91fe829e6a501622
Ege Özel <egexozel@gmail.com>
2025-04-13 22:42:38 +0300

    last touches

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Tabs from './Tabs';
+
 import ClassProgress from './ClassProgress';
 import './styles.css';
 
--- src/BookProgress.js
+++ src/BookProgress.js
@@ -6,9 +6,9 @@ const BookProgress = ({ book, progress, updateProgress }) => {
     <div className="book-progress">
       <h3>{book}</h3>
       <div className="controls" style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-        <FaMinusCircle onClick={(event) => { event.stopPropagation(); updateProgress(book, -1); }} style={{ color: 'red', cursor: 'pointer' }} />
+        <FaMinusCircle onClick={(event) => { event.stopPropagation(); updateProgress(book, -1); }} style={{ color: '#d1133f', cursor: 'pointer' }} />
         <p>Tests Completed: {progress}</p>
-        <FaPlusCircle onClick={(event) => { event.stopPropagation(); updateProgress(book, 1); }} style={{ color: 'green', cursor: 'pointer' }} />
+        <FaPlusCircle onClick={(event) => { event.stopPropagation(); updateProgress(book, 1); }} style={{ color: '#88d113', cursor: 'pointer' }} />
       </div>
     </div>
   );
